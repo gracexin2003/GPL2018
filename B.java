098d7e88dc62cf2070579a32c476b4a3180188f2
Grace Xin <gracexin2003@users.noreply.github.com>
2018-09-08 17:41:48 -0700

    B

---
+++ B.java
@@ -0,0 +1,15 @@
+package GPL2018;
+
+import java.util.*;
+
+@SuppressWarnings ("resource")
+class B {
+
+    public static void main (String[] args) throws Exception {
+    	
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt()*s.nextInt()*s.nextInt());
+        
+    }
+
+}
