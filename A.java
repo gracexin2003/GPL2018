3b740cf5ee8893c0d996f3377356aeff81d1aeae
Grace Xin <gracexin2003@users.noreply.github.com>
2018-09-08 17:41:38 -0700

    A

---
+++ A.java
@@ -0,0 +1,16 @@
+package GPL2018;
+
+import java.util.*;
+
+@SuppressWarnings ("resource")
+public class A {
+
+    public static void main (String[] args) {
+    	
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.println(str+str);
+        
+    }
+
+}
